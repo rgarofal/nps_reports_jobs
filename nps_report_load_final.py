5151e8d6e81c539d10f5ee95c071914dfba00e52
rgarofal <roberto.rgarofalo@gmail.com>
2018-05-09 17:34:06 +0200

    Aggiornata lista di mail per Assistenza Tecnica OCS e Amministrativa

--- nps_report_load_final.py
+++ nps_report_load_final.py
@@ -22,10 +22,19 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--directory_report',
                         default='C:\\Users\\rogarofalo\\Documents\\WorkingEnv\\NPS_ASSISTENZA_TECNICA\\Working_AssistenzaTecnica\\REPORT_GIORNALIERI',
                         help='Directory dove risiedono i report ', required=False)
+    parser.add_argument('-r', '--report',
+                        default='TUTTI',
+                        help='Tipologia Report: TUTTI (OCS ASS Tecnica/OCS_TEC/OCS_AMM), ASS_TEC Assistenza Tecnica , OCS_TEC Single Contact Tecnica, OCS_AMM Single Contact Amministrativa ', required=False)
+    parser.add_argument('-t', '--test_mail',
+                        default='FALSE',
+                        help='FALSE usa le mailing list ufficiali; TRUE us ale mailing list di test ',
+                        required=False)
 
     args = parser.parse_args()
 
     directory = args.directory_report
+    tipo_report = args.report
+    test = args.test_mail
     print(directory)
 
     # import textwrap
@@ -46,59 +55,67 @@ if __name__ == '__main__':
     now = datetime.today() - timedelta(days=days_to_subtract)
     time_label = now.strftime("%d-%m-%Y")
 
+    if tipo_report == 'ASS_TEC' or tipo_report == 'TUTTI':
 
-    # REPORT ASSISTENZA TECNICA
-    report = ConcreteBaseReportNPS(directory, 0)
-    dao = ConcreteDatabaseMySQL()
-    dao.connection()
-    report.produce_reports_csv(dao)
-    report.produce_zip_report('report_NPS_assistenza_tecnica.zip')
-    lista_mail = ['giovanni.laforgia@fastweb.it', 'roberto.garofalo@consulenti.fastweb.it', 'giovanni.galgano@fastweb.it', 'roberto.garofalo@spindox.it', 'vincenzo.fioretti@fastweb.it', 'alessio.garbetta@fastweb.it']
-    # lista_mail = ['roberto.garofalo@consulenti.fastweb.it','roberto.garofalo@spindox.it']
-
-
-    subject = '{}{!s}{}'.format('Liste NPS_assistenza_tecnica ', time_label, ' -Caricamento coerente ')
-
-    # message = textwrap.dedent(decorator.format(file_1, file_2, file_3))
-    mail = ConcreteCreatorMailerNPS(lista_mail)
-    mail.set_message(message, subject)
-    nome_file_zip = report.get_file_name_zip()
-    mail.set_attachment(nome_file_zip)
-    mail.send_mail()
-
-    # REPORT ASSISTENZA SINGLE CONTACT TECNICA
-    report = ConcreteBaseReportNPS_OCS_TECH(directory, 0)
-    dao = ConcreteDatabaseMySQL()
-    dao.connection()
-    report.produce_reports_csv(dao)
-    report.produce_zip_report('report_NPS_OCS_assistenza_tecnica.zip')
-    lista_mail = ['giovanni.laforgia@fastweb.it', 'roberto.garofalo@consulenti.fastweb.it', 'giovanni.galgano@fastweb.it', 'roberto.garofalo@spindox.it', 'vincenzo.fioretti@fastweb.it', 'alessio.garbetta@fastweb.it']
-    # lista_mail = ['roberto.garofalo@consulenti.fastweb.it', 'roberto.garofalo@spindox.it']
-
-    subject = '{}{!s}{}'.format('Liste NPS_Single Contact assistenza_tecnica ', time_label, ' -Caricamento coerente ')
-
-    # message = textwrap.dedent(decorator.format(file_1, file_2, file_3))
-    mail = ConcreteCreatorMailerNPS(lista_mail)
-    mail.set_message(message, subject)
-    nome_file_zip = report.get_file_name_zip()
-    mail.set_attachment(nome_file_zip)
-    mail.send_mail()
+       # REPORT ASSISTENZA TECNICA
+       report = ConcreteBaseReportNPS(directory, 0)
+       dao = ConcreteDatabaseMySQL()
+       dao.connection()
+       report.produce_reports_csv(dao)
+       report.produce_zip_report('report_NPS_assistenza_tecnica.zip')
+       if test == 'FALSE':
+          lista_mail = ['giovanni.laforgia@fastweb.it', 'roberto.garofalo@consulenti.fastweb.it', 'giovanni.galgano@fastweb.it', 'roberto.garofalo@spindox.it', 'vincenzo.fioretti@fastweb.it', 'alessio.garbetta@fastweb.it']
+       else:
+          lista_mail = ['roberto.garofalo@consulenti.fastweb.it','roberto.garofalo@spindox.it']
 
 
-    # REPORT AMMINISTRATIVA SINGLE CONTACT
-    report = ConcreteBaseReportNPS_OCS_AMM(directory, 0)
-    dao = ConcreteDatabaseMySQL()
-    dao.connection()
-    report.produce_reports_csv(dao)
-    report.produce_zip_report('report_NPS_OCS_amministrativa.zip')
-    lista_mail = ['giovanni.laforgia@fastweb.it', 'roberto.garofalo@consulenti.fastweb.it', 'giovanni.galgano@fastweb.it', 'roberto.garofalo@spindox.it', 'arduino.panaro@fastweb.it', 'romana.mastrandrea@fastweb.it']
-    # lista_mail = ['roberto.garofalo@consulenti.fastweb.it','roberto.garofalo@spindox.it']
+       subject = '{}{!s}{}'.format('Liste NPS_assistenza_tecnica ', time_label, ' -Caricamento coerente ')
 
-    subject = '{}{!s}{}'.format('Liste NPS_Single Contact Amministrativa ', time_label, ' -Caricamento coerente ')
+       # message = textwrap.dedent(decorator.format(file_1, file_2, file_3))
+       mail = ConcreteCreatorMailerNPS(lista_mail)
+       mail.set_message(message, subject)
+       nome_file_zip = report.get_file_name_zip()
+       mail.set_attachment(nome_file_zip)
+       mail.send_mail()
 
-    # message = textwrap.dedent(decorator.format(file_1, file_2, file_3))
-    mail = ConcreteCreatorMailerNPS(lista_mail)
-    mail.set_message(message, subject)
-    nome_file_zip = report.get_file_name_zip()
-    mail.set_attachment(nome_file_zip)
-    mail.send_mail()
+    if tipo_report == 'OCS_TEC' or tipo_report == 'TUTTI':
+       # REPORT ASSISTENZA SINGLE CONTACT TECNICA
+       report = ConcreteBaseReportNPS_OCS_TECH(directory, 0)
+       dao = ConcreteDatabaseMySQL()
+       dao.connection()
+       report.produce_reports_csv(dao)
+       report.produce_zip_report('report_NPS_OCS_assistenza_tecnica.zip')
+       if test == 'FALSE':
+          lista_mail = ['giovanni.laforgia@fastweb.it', 'roberto.garofalo@consulenti.fastweb.it', 'giovanni.galgano@fastweb.it', 'roberto.garofalo@spindox.it', 'vincenzo.fioretti@fastweb.it', 'alessio.garbetta@fastweb.it']
+       else:
+          lista_mail = ['roberto.garofalo@consulenti.fastweb.it', 'roberto.garofalo@spindox.it']
+
+       subject = '{}{!s}{}'.format('Liste NPS_Single Contact assistenza_tecnica ', time_label, ' -Caricamento coerente ')
+
+       # message = textwrap.dedent(decorator.format(file_1, file_2, file_3))
+       mail = ConcreteCreatorMailerNPS(lista_mail)
+       mail.set_message(message, subject)
+       nome_file_zip = report.get_file_name_zip()
+       mail.set_attachment(nome_file_zip)
+       mail.send_mail()
+    if tipo_report == 'OCS_AMM' or tipo_report == 'TUTTI':
+
+       # REPORT AMMINISTRATIVA SINGLE CONTACT
+       report = ConcreteBaseReportNPS_OCS_AMM(directory, 0)
+       dao = ConcreteDatabaseMySQL()
+       dao.connection()
+       report.produce_reports_csv(dao)
+       report.produce_zip_report('report_NPS_OCS_amministrativa.zip')
+       if test == 'FALSE':
+          lista_mail = ['giovanni.laforgia@fastweb.it', 'roberto.garofalo@consulenti.fastweb.it', 'giovanni.galgano@fastweb.it', 'roberto.garofalo@spindox.it', 'arduino.panaro@fastweb.it', 'romana.mastrandrea@fastweb.it']
+       else:
+          lista_mail = ['roberto.garofalo@consulenti.fastweb.it','roberto.garofalo@spindox.it']
+
+       subject = '{}{!s}{}'.format('Liste NPS_Single Contact Amministrativa ', time_label, ' -Caricamento coerente ')
+
+       # message = textwrap.dedent(decorator.format(file_1, file_2, file_3))
+       mail = ConcreteCreatorMailerNPS(lista_mail)
+       mail.set_message(message, subject)
+       nome_file_zip = report.get_file_name_zip()
+       mail.set_attachment(nome_file_zip)
+       mail.send_mail()
