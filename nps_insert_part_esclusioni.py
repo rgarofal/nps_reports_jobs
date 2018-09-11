3eb2bb66d46bf84169dcb17e7dc03980a2cae69c
rgarofal <roberto.rgarofalo@gmail.com>
2018-09-11 12:57:01 +0200

    modulo per insert massivo su tabella partizionata prova inserimento per uno specifico valore

--- nps_insert_part_esclusioni.py
+++ nps_insert_part_esclusioni.py
@@ -1,10 +1,7 @@
 from __future__ import print_function
-from datetime import date, datetime, timedelta
+
 import mysql.connector
 import argparse
-
-import csv
-from datetime import datetime, timedelta
 
 
 def help_msg():
@@ -17,7 +14,7 @@ def help_msg():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=help_msg())
     parser.add_argument('-d', '--directory_report',
-                        default='C:\\Users\\rogarofalo\\Documents\\WorkingEnv\\NPS_ASSISTENZA_TECNICA\\Working_AssistenzaTecnica\\REPORT_GIORNALIERI',
+                        default='C:\\Users\\rogarofalo\\Documents\\REPORT_INSERT',
                         help='Directory dove risiedono i report ', required=False)
 
     parser.add_argument('-t', '--table',
@@ -37,24 +34,6 @@ if __name__ == '__main__':
     print("Tabella selezionata = ", table)
     print("Colonne = ", colonne)
 
-    # import textwrap
-    #
-    # PARTE COMUNE PER i REPORT
-    #
-    decorator = """\
-                   Ciao a tutti
-
-                   Come richiesto vi invio i reports in formato zip 
-
-                   A disposizione
-
-                   Roberto
-                  """
-    message = decorator
-    days_to_subtract = 0
-    now = datetime.today() - timedelta(days=days_to_subtract)
-    time_label = now.strftime("%d-%m-%Y")
-
     host_n = "db801rco.intranet.fw"  # your host
     user_n = "rogarofalo"  # username
     passwd_n = "R0garof4lo"  # password
@@ -64,12 +43,8 @@ if __name__ == '__main__':
     cnx = mysql.connector.connect(host=host_n, user=user_n, password=passwd_n, database=db_n)
     cursor = cnx.cursor()
 
-    # dao = ConcreteDatabaseMySQL()
-    # dao.connection()
-    # insert into trb2 (id, fname, lname, hired,separated,job_code, store_id) values (2,'pippo3', 'pluto3', date(now() - interval 2 year), date(now()), 12, 13);
     for k in range(1, 8):
         for i in range(1, 1000000):
-            # statement = 'insert into {1} ({2}) values ({3}, ''username_{4}'', ''surname_{5}'', date(now() - interval {6} year),date(now()), 12, 13)',format(table,colonne ,i, num, num, yy)
             data = '(%s, "%s")' % (i, flusso_str[k])
             print(data)
             # print(statement)
